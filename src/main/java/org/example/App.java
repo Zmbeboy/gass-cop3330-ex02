9c56e3350f43908572bbf57dd3af95f230af22bc
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2021-09-02 14:24:35 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Eric Gass
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String input = scan.nextLine();
+        int strlen = input.length();
+        System.out.println(input+" has "+strlen+" characters.");
+    }
+}
